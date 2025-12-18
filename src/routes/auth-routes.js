3af06b86c46986fbc2dec12a50885d9f3e4912b8
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-19 02:33:08 +0300

    fix naming

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import db from '../db.js';
-import { register, login } from '../controllers/auth-controllers.js';
+import { register, login } from '../controllers/auth-controller.js';
 const router = express.Router();
 
 router.post('/register', register);
