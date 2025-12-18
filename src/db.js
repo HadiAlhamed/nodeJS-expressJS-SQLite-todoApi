9ee982a2c6f877dc23474d806cc1feb96fa88761
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-18 03:13:21 +0300

    bugfix:delete extra comma

--- src/db.js
+++ src/db.js
@@ -8,8 +8,8 @@ db.exec(
   `
     CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username UNIQUE TEXT NOT NULL,
-        password TEXT NOT NULL,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
     )
 
 `
